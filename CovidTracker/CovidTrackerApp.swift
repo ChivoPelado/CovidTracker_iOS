16444f345228a14f1130ef6c6391275f87cb21e6
Andres Herrera <aherrera@gcv.ec>
2021-01-31 20:47:03 -0500

    Initial Commit

---
+++ CovidTracker/CovidTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidTrackerApp.swift
+//  CovidTracker
+//
+//  Created by Andres Herrera on 1/31/21.
+//
+
+import SwiftUI
+
+@main
+struct CovidTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
